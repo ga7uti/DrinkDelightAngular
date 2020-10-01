d6644eb1f5058691ebcdebeac17dd671787e5228
gauti <gauti783@gmail.com>
2020-10-01 12:28:56 +0530

    fix

--- src/app/component/user/place-orders/place-orders.component.ts
+++ src/app/component/user/place-orders/place-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductOrder } from 'src/app/models/ProductOrder';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-place-orders',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceOrdersComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('f', { static: false }) signupForm: NgForm;
+  names = ['a', 'b'];
+  distributors: Distributor[];
+  warehouses: Warehouse[];
+  order: ProductOrder = new ProductOrder();
+  orderId: Number;
+  name: string;
+  distributor: Distributor;
+  warehouse: Warehouse;
+  quantityUnit: number;
+  pricePerUnit: number;
+  dateOfDelivery: Date;
+  submitted = false;
 
+  constructor(private productService: ProductService) {
+    this.distributors = productService.getdistributors();
+    this.warehouses = productService.getWarehouses();
+    this.names=productService.getProductStockNames();
+  }
   ngOnInit(): void {
+  }
+  postOrder(order: ProductOrder): Number {
+    this.productService.postOrderObserveable(order).subscribe(res => { this.orderId = res.orderId; });
+    return this.orderId;
+  }
+  onSubmit() {
+    this.order.distributor = this.distributor;
+    this.order.warehouse = this.warehouse;
+    this.order.name = this.name;
+    this.order.pricePerUnit = this.pricePerUnit;
+    this.order.quantityUnit = this.quantityUnit;
+    this.order.dateOfDelivery = this.dateOfDelivery;
+    this.orderId = this.postOrder(this.order);
+    this.submitted = true;
+    this.signupForm.reset();
   }
 
 }
